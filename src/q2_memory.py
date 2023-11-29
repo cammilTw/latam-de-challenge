2debb84f96bf82a2c0a28334b030951513323ffd
Cammil Guzmán <cammil.guzman@thoughtworks.com>
2023-11-29 16:01:58 -0300

    q2_memory solved

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,19 @@
 from typing import List, Tuple
+from emoji import EMOJI_DATA
+import json
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    #Keep track of emojis
+    emojiCount = {}
+
+    #Read file, by line
+    with open(file_path, "r") as f:
+        for line in f:
+            #Parse line, find emojis and add them to the tracker
+            emojis = [c for c in json.loads(line)['content'] if c in EMOJI_DATA]
+            for emoji in emojis:
+                emojiCount[emoji] = emojiCount.get(emoji, 0) + 1
+
+    #Sort emojis by count
+    sortedEmojis = sorted(emojiCount, key=lambda e: emojiCount[e], reverse=True)[0:10]
+    return [(emoji, emojiCount[emoji]) for emoji in sortedEmojis]
